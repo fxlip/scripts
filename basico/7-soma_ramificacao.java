e0c7a2f5505365d82489dc3fb23bf4b2f9348f6d
fxlip <felip42@gmail.com>
2023-04-28 18:23:21 -0300

    soma ramificacao

---
+++ basico/7-soma_ramificacao.java
@@ -0,0 +1,14 @@
+/*
+
+Função que recebe uma Árvore Binária e retorna uma lista das somas de seus ramos.
+Ordenadas da soma do ramo mais à esquerda até a soma do ramo mais à direita.
+
+Uma soma de ramo é a soma de todos os valores em um ramo de Árvore Binária. 
+Um ramo de Árvore Binária é um caminho de nós em uma árvore que começa no nó raiz e termina em qualquer nó folha.
+
+Cada nó de BinaryTree tem um valor inteiro, um nó filho esquerdo e um nó filho direito. 
+Os nós filhos podem ser nós de BinaryTree ou none/null.
+
+*/
+import java.util.*;
+
