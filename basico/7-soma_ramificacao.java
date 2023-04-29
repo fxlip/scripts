e6ba748549e4d8edfe5412442991cf952f2faab4
fxlip <felip42@gmail.com>
2023-04-29 08:46:16 -0300

    somas de ramificacao

--- basico/7-soma_ramificacao.java
+++ basico/7-soma_ramificacao.java
@@ -12,3 +12,44 @@ Os nós filhos podem ser nós de BinaryTree ou none/null.
 */
 import java.util.*;
 
+class Program {
+    // Esta é a classe do nó raiz de entrada.
+    public static class BinaryTree {
+        int value;
+        BinaryTree left;
+        BinaryTree right;
+
+        BinaryTree(int value) {
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    // Função principal para calcular as somas dos ramos
+    public static List<Integer> branchSums(BinaryTree root) {
+        List<Integer> sums = new ArrayList<Integer>(); // Inicializa a lista de somas
+        calculateBranchSums(root, 0, sums); // Chama a função recursiva para calcular as somas
+        return sums; // Retorna a lista de somas
+    }
+
+    // Função recursiva para calcular as somas dos ramos
+    public static void calculateBranchSums(BinaryTree node, int runningSum, List<Integer> sums) {
+        if (node == null) { // Caso base: se o nó for nulo, retorne
+            return;
+        }
+
+        // Atualiza a soma com o valor do nó atual
+        int newRunningSum = runningSum + node.value;
+
+        // Se o nó for uma folha (não tiver filhos à esquerda ou à direita)
+        if (node.left == null && node.right == null) {
+            sums.add(newRunningSum); // Adiciona a soma atualizada à lista de somas
+            return;
+        }
+
+        // Chamadas recursivas para os nós filhos à esquerda e à direita
+        calculateBranchSums(node.left, newRunningSum, sums);
+        calculateBranchSums(node.right, newRunningSum, sums);
+    }
+}
