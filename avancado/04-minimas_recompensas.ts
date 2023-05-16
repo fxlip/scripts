3ff1ea7b4f7b19cf54b9ed57fff33da5de7afe3f
fxlip <felip42@gmail.com>
2023-05-16 12:13:33 -0300

    maior intervalo

--- avancado/04-minimas_recompensas.ts
+++ avancado/04-minimas_recompensas.ts
@@ -13,3 +13,27 @@ Você decide fazer isso de maneira justa, dando-lhes recompensas arbitrárias se
 com uma nota menor e deve receber estritamente menos recompensas do que um aluno adjacente com uma nota maior.
 
 */
+
+export function minRewards(scores: number[]): number {
+    // Inicializa o array de recompensas com 1 para cada aluno
+    const rewards = scores.map(_ => 1);
+  
+    // Percorre o array da esquerda para a direita
+    for (let i = 1; i < scores.length; i++) {
+      // Se a nota do aluno atual é maior que a nota do aluno anterior, aumenta sua recompensa
+      if (scores[i] > scores[i - 1]) {
+        rewards[i] = rewards[i - 1] + 1;
+      }
+    }
+  
+    // Percorre o array da direita para a esquerda
+    for (let i = scores.length - 2; i >= 0; i--) {
+      // Se a nota do aluno atual é maior que a nota do aluno seguinte, aumenta sua recompensa
+      if (scores[i] > scores[i + 1]) {
+        rewards[i] = Math.max(rewards[i], rewards[i + 1] + 1);
+      }
+    }
+  
+    // Retorna a soma das recompensas
+    return rewards.reduce((a, b) => a + b);
+}
