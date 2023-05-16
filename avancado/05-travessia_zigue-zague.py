4013283c33caa33b13e26bc2b78e1acb059e946d
fxlip <felip42@gmail.com>
2023-05-16 12:23:51 -0300

    file travessia em zigue-zague

---
+++ avancado/05-travessia_zigue-zague.py
@@ -0,0 +1,9 @@
+"""
+
+Função que recebe um array bidimensional de tamanho n x m (que pode ser quadrado quando n == m) 
+e retorna um array unidimensional com todos os elementos do array em ordem de zigue-zague.
+
+A ordem de zigue-zague começa no canto superior esquerdo do array bidimensional, 
+desce um elemento e prossegue em um padrão de zigue-zague até o canto inferior direito.
+
+"""
