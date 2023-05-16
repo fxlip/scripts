36b8143adb6625a4cf6c0fa2c2b02c3ea0aaca39
fxlip <felip42@gmail.com>
2023-05-16 08:09:48 -0300

    descricao array monotonico

---
+++ intermediario/04-array_monotonico.swift
@@ -0,0 +1,10 @@
+/*
+
+Função que recebe um array de inteiros e retorna um booleano representando se o array é monotônico ou não.
+
+Monotônico: se seus elementos, da esquerda para a direita, são inteiramente não-crescentes ou inteiramente não-decrescentes.
+
+Elementos não-crescentes: não são necessariamente exclusivamente decrescentes; eles simplesmente não aumentam. 
+Elementos não-decrescentes: não são necessariamente exclusivamente crescentes; eles simplesmente não diminuem.
+
+*/
