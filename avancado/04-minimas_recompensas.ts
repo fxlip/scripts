8f0216a95b810d4335a823fdcfbe2b1dd9be17ba
fxlip <felip42@gmail.com>
2023-05-16 12:07:48 -0300

    arquivo maior intervalo

---
+++ avancado/04-minimas_recompensas.ts
@@ -0,0 +1,15 @@
+/*
+
+Função que recebe uma lista de notas e retorna o número mínimo de recompensas que pode ser dada aos alunos
+
+Imagine que você é um professor que acabou de corrigir o exame final de uma turma. 
+Você tem uma lista de notas dos alunos no exame final em uma ordem específica (não necessariamente ordenada) 
+e deseja recompensar seus alunos. 
+
+Você decide fazer isso de maneira justa, dando-lhes recompensas arbitrárias seguindo duas regras:
+
+1. Todos os alunos devem receber pelo menos uma recompensa.
+2. Qualquer aluno deve receber estritamente mais recompensas do que um aluno adjacente (um aluno imediatamente à esquerda ou à direita) 
+com uma nota menor e deve receber estritamente menos recompensas do que um aluno adjacente com uma nota maior.
+
+*/
