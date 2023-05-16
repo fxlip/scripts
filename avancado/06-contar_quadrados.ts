117a8b6c2bfbccdc47a928ef81bbd79ee16e3168
fxlip <felip42@gmail.com>
2023-05-16 12:39:10 -0300

    init contar quadrados

---
+++ avancado/06-contar_quadrados.ts
@@ -0,0 +1,10 @@
+/*
+
+Função que recebe uma lista de coordenadas cartesianas e retorna o número de quadrados que podem ser formados por essas coordenadas.
+
+Um quadrado deve ter seus quatro cantos entre as coordenadas para ser contado. 
+
+Uma única coordenada pode ser usada como canto para vários quadrados diferentes.
+
+*/
+
