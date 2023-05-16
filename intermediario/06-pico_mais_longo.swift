ff0df1a33a55056028bf0a5d810d1413354fd4bc
fxlip <felip42@gmail.com>
2023-05-16 09:01:11 -0300

    arquivo pico mais longo

---
+++ intermediario/06-pico_mais_longo.swift
@@ -0,0 +1,13 @@
+/*
+
+Função que recebe um array de inteiros e retorna o comprimento do pico mais longo no array.
+
+Um pico é definido como inteiros adjacentes no array que são estritamente crescentes 
+até atingirem um ponto máximo (o valor mais alto do pico), a partir do qual se tornam estritamente decrescentes. 
+
+Pelo menos três inteiros são necessários para formar um pico.
+
+Exemplo: os inteiros 1, 4, 10, 2 formam um pico, mas os inteiros 4, 0, 10 não e nem os inteiros 1, 2, 2, 0. 
+Da mesma forma, os inteiros 1, 2, 3 não formam um pico porque não há inteiros estritamente decrescentes após o 3.
+
+*/
