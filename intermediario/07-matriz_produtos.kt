d134360304874ddf1cc8bb46723c3a80612e3483
fxlip <felip42@gmail.com>
2023-05-17 10:52:41 -0300

    file matriz de produtos

---
+++ intermediario/07-matriz_produtos.kt
@@ -0,0 +1,10 @@
+/*
+
+Input: matriz não vazia de inteiros;
+Output: matriz de mesmo tamanho;
+        cada elemento é igual ao produto de todos os outros números na matriz de entrada
+
+
+valor em output[i] é igual ao produto de todos os números na matriz de entrada exceto input[i]
+
+*/
